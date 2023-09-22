8454ec5671c11b627ecdc43613ba43b11d5a0468
Case Wylie <cmwylie19@defenseunicorns.com>
2023-09-21 22:45:47 -0400

    updates
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- cashcorns-backend/pkg/server/server.go
+++ cashcorns-backend/pkg/server/server.go
@@ -56,16 +56,14 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Handle file upload
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the form data
+
 	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Get the uploaded file
 	file, _, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -73,7 +71,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	// Decode the JSON file
 	var data struct {
 		Leaderboard []struct {
 			Username string `json:"username"`
@@ -85,7 +82,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create a CSV file
 	csvFile, err := os.Create("output.csv")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -93,7 +89,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer csvFile.Close()
 
-	// Write CSV data
 	csvWriter := csv.NewWriter(csvFile)
 	_ = csvWriter.Write([]string{"Rippling Emp No", "Employee Name", "Mission Hero", "Project", "Service Item", "Shift ID", "Salary Hours", "Salary Amount", "Dental Insurance (Imputed)", "Expenses Reimbursement", "Life Insurance (Imputed)", "Medical Insurance (Imputed)", "NSO Exercise Income", "Office Expense Reimbursement (Recurring)", "Reimbursements", "Taco Bonus", "Vision Insurance (Imputed)", "Dental Deductions (Dental) Emp. Deduction", "Dental Deductions (Dental) Co. Contribution", "401K (Pre-tax %) (401K) Emp. Deduction", "401K (Pre-tax %) (401K) Co. Contribution", "Vision Deductions (Vision) Emp. Deduction", "Vision Deductions (Vision) Co. Contribution", "Medical Deductions (Medical) Emp. Deduction", "Medical Deductions (Medical) Co. Contribution", "Spend Management - Card Repayment (Post Tax) Emp. Deduction", "Spend Management - Card Repayment (Post Tax) Co. Contribution", "401K (Post-tax %) (Roth 401K) Emp. Deduction", "401K (Post-tax %) (Roth 401K) Co. Contribution", "Life Deductions (Life) Emp. Deduction", "Life Deductions (Life) Co. Contribution", "Long Term Disability Deductions (Long Term Disability) Emp. Deduction", "Long Term Disability Deductions (Long Term Disability) Co. Contribution", "Payroll Correction (Post Tax) Emp. Deduction", "Payroll Correction (Post Tax) Co. Contribution", "HSA (HSA) Emp. Deduction", "HSA (HSA) Co. Contribution", "Short Term Disability Deductions (Short Term Disability) Emp. Deduction", "Short Term Disability Deductions (Short Term Disability) Co. Contribution", "Loan Repayment (Post-Tax) (Post Tax) Emp. Deduction", "Loan Repayment (Post-Tax) (Post Tax) Co. Contribution", "Negative Net Pay Payment Plan (Post Tax) Emp. Deduction", "Negative Net Pay Payment Plan (Post Tax) Co. Contribution", "Michael N Palassis_Case ID: 7105103332", "Paystub Note"})
 	for _, item := range data.Leaderboard {
@@ -109,42 +104,37 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer csvWriter.Flush()
 
-	// Check for errors in writing CSV
 	if err := csvWriter.Error(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// _ = removeDoubleQuotesFromFile("output.csv")
 
-	// Respond with the download link
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`<html>
 	<head>
+	<title>🦄</title>
 	<style>
 	body {
-		/* Set the background image URL */
+	
 		background-image: url('https://www.defenseunicorns.com/static/home-hero-bg-feff54111a83d6b7c0c38219c80b1333.jpg');
 	
-		/* Set the background image size */
 		background-size: cover;
-	
-		/* Center the background image horizontally and vertically */
+		font-family: sans-serif;
 		background-position: center;
 	
-		/* Make the background image fixed (doesn't scroll with the content) */
 		background-attachment: fixed;
 	
-		/* Specify a background color in case the image doesn't load or cover the entire viewport */
-		background-color: #f0f0f0; /* Replace with your desired background color */
+
+		background-color: #f0f0f0; 
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
 	a {
 		text-decoration: none;
-		color: #fff;
+		color: red;
 		font-size: 2rem;
-		transition: transform 0.3s ease-in-out;
+		transition: transform 1.5s ease-in-out;
 	}
 	a:hover {
 		transform: scale(1.2) rotate(360deg);
@@ -152,7 +142,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	</style>
 	</head>
 	
-	<body><a href=\"/download\">Download Me 🦄</a></body></html>`))
+	<body><a href="/download">Download Me 🦄</a></body></html>`))
 }
 
 func removeDoubleQuotesFromFile(filePath string) error {
@@ -163,7 +153,6 @@ func removeDoubleQuotesFromFile(filePath string) error {
 	}
 	defer file.Close()
 
-	// Create a temporary file to store the modified content
 	tempFilePath := filePath + ".temp"
 	tempFile, err := os.Create(tempFilePath)
 	if err != nil {
@@ -171,16 +160,13 @@ func removeDoubleQuotesFromFile(filePath string) error {
 	}
 	defer tempFile.Close()
 
-	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Remove double quotes from the line
 		modifiedLine := strings.Replace(line, `"`, "", -1)
 
-		// Write the modified line to the temporary file
 		_, err := tempFile.WriteString(modifiedLine + "\n")
 		if err != nil {
 			return err
@@ -203,10 +189,10 @@ func removeDoubleQuotesFromFile(filePath string) error {
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
-	// Specify the file you want to send for download
-	filePath := "output.csv" // Replace with the path to your file
+
+	filePath := "output.csv"
 	_ = removeDoubleQuotesFromFile(filePath)
-	// Open the file for reading
+
 	file, err := os.Open(filePath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -214,13 +200,9 @@ func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	// Set the Content-Disposition header to suggest a filename for the download
 	w.Header().Set("Content-Disposition", "attachment; filename="+filePath)
 
-	// Set the Content-Type header based on the file's type or specify "application/octet-stream" for binary files
-	w.Header().Set("Content-Type", "text/plain") // Replace with the appropriate MIME type
-
-	// Copy the file's content to the HTTP response
+	w.Header().Set("Content-Type", "text/csv")
 	_, err = io.Copy(w, file)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
